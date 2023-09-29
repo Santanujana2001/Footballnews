30f6aac51e56b435988a442b1b0c7c767e879d09
Santanu Jana <sanujana7551@gmail.com>
2023-09-30 01:23:02 +0530

    about me

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
   <nav className="grid grid-flow-col gap-4">
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
+    <Link to={{pathname:"fanciful-croissant-f596a9.netlify.app/"}} target="_blank" className="link link-hover">About me</Link> 
+    <Link className="link link-hover">Contact</Link> 
+    <Link className="link link-hover">Github</Link>
   </nav> 
   <nav>
     <div className="grid grid-flow-col gap-4">
@@ -17,7 +17,7 @@ function Footer() {
     </div>
   </nav> 
   <aside>
-    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © 2023 - All right reserved by Santanu Jana</p>
   </aside>
 </footer>
   )
